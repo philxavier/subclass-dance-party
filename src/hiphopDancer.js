48be2ae27ade7c96cb941f8a8bd1258f0f24ec9d
Christopher Weilacker <chrisweilacker@gmail.com>
2018-10-25 18:54:43 -0700

    Complete Advanced Content

--- src/hiphopDancer.js
+++ src/hiphopDancer.js
@@ -1,28 +1,26 @@
-//Pseudoclassical Hip Hop Dancer
-var MakeHipHopDancer = function(top, left, timeBetweenSteps) {
-  MakeDancer.call(this, top, left, timeBetweenSteps);
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
-  this.$node = $('<img class="hiphop-dancer" src="img/hiphop-left.png"></img>');
+class MakeHipHopDancer extends MakeDancer {
+  constructor (top, left, timeBetweenSteps) {
+    // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+    // so we must keep a copy of the old version of this function
+    super(top, left, timeBetweenSteps);
+    this.$node = $('<img class="hiphop-dancer" src="img/hiphop-left.png"></img>');
+    this.setPosition(top, left);
+    this.animate();
 
-  this.setPosition(top, left);
-  this.animate();
-};
+  }
+  step () {
 
-MakeHipHopDancer.prototype = Object.create(MakeDancer.prototype);
-MakeHipHopDancer.prototype.constructor = MakeHipHopDancer;
+    super.step();
 
-MakeHipHopDancer.prototype.step = function() {
-
-  MakeDancer.prototype.step.call(this);
-  // toggle() is a jQuery method to show/hide the <span> tag.
-  // See http://api.jquery.com/category/effects/ for this and
-  // other effects you can use on a jQuery-wrapped html tag.
-  if (this.$node.attr('src') === 'img/hiphop-left.png') {
-    this.$node.attr('src', 'img/hiphop-right.png');
-  } else if (this.$node.attr('src') === 'img/hiphop-right.png') {
-    this.$node.attr('src', 'img/hiphop-left.png');
+    if (this.$node.attr('src') === 'img/hiphop-left.png') {
+      this.$node.attr('src', 'img/hiphop-right.png');
+    } else if (this.$node.attr('src') === 'img/hiphop-right.png') {
+      this.$node.attr('src', 'img/hiphop-left.png');
+    }
   }
 
-};
+}
+
+window.MakeHipHopDancer = MakeHipHopDancer;
+
 
